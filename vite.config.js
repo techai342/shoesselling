19a636942100cb912444c6dcbcadade3817f8035
techai342 <tiktokusr4400@gmail.com>
2025-11-27 15:50:34 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 3000
+  }
 })
